cefec991935be460964b067415d038993234e7ad
Amar <spidermine63@gmail.com>
2016-10-19 00:19:22 +0000

    First commit!

--- language_detector/languages.py
+++ language_detector/languages.py
@@ -26,6 +26,27 @@ LANGUAGES = [
             'einen', 'kann', 'dem'
         ]
     },
+    {
+        'name': 'English',
+        'common_words': [
+            'time', 'person', 'year', 'way', 'day', 'thing', 'man', 
+            'world', 'life', 'hand', 'part', 'child', 'eye', 'woman', 
+            'place', 'work', 'week', 'case', 'point', 'government', 
+            'company', 'number', 'group', 'problem', 'fact', 'be', 
+            'have', 'do', 'say', 'get', 'make', 'go', 'know', 'take', 
+            'see', 'come', 'think', 'look', 'want', 'give', 'use', 'find', 
+            'tell', 'ask', 'work', 'seem', 'feel', 'try', 'leave', 'call', 
+            'good', 'new', 'first', 'last', 'long', 'great', 'little', 
+            'own', 'other', 'old', 'right', 'big', 'high', 'different', 
+            'small', 'large', 'next', 'early', 'young', 'important', 'few', 
+            'public', 'bad', 'same', 'able', 'to', 'of', 'in', 'for', 'on', 
+            'with', 'at', 'by', 'from', 'up', 'about', 'into', 'over', 'after', 
+            'beneath', 'under', 'above', 'Others', 'the', 'and', 'a', 'that', 
+            'I', 'it', 'not', 'he', 'as', 'you', 'this', 'but', 'his', 'they', 
+            'her', 'she', 'or', 'an', 'will', 'my', 'one', 'all', 'would', 
+            'there', 'their'
+        ]
+    }
     #  include the English language here
     # HINT: https://en.wikipedia.org/wiki/Most_common_words_in_English
 ]
--- language_detector/main.py
+++ language_detector/main.py
@@ -5,4 +5,30 @@
 def detect_language(text, languages):
     """Returns the detected language of given text."""
     # implement your solution here
-    pass
+    words = text.split()
+    
+    language_guess = ''
+    valid_word_max = 0
+    num_words = {}
+    for language in languages:
+        # The languages are stored in a list of dictionaries
+        # we will go through each language checking for valid words.
+        valid_words = 0
+        for word in words:
+            word = word.lower()
+            word = word.strip(" !@#$%^&*()-_+={}[]|\:;'<>?,./\"")
+            
+            if word in language['common_words']:
+                valid_words += 1
+                
+        #compares valid_words to select the largest and updates language_guess 
+        if valid_words > valid_word_max: 
+            valid_word_max = valid_words
+            language_guess = language['name']
+        
+        #for word in words:
+            
+            
+    return language_guess
+        
+        
